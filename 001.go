49c43d130ba14b78074d8db1ee057b19e7e701fe
Alex Wong <gmem@me.com>
2021-10-14 14:20:06 +0800

    initial commit

--- 001.go
+++ 001.go
@@ -2,11 +2,16 @@ package main
 
 func main() {
 	oldNums := []int{1, 2, 3}
-	var newNums []*int
-	for _, num := range oldNums {
-		newNums = append(newNums, &num)
-	}
+	newNums := toPtrs(oldNums)
 	for _, num := range newNums {
 		print(*num)
 	}
+}
+
+func toPtrs(nums []int) []*int {
+	var newNums []*int
+	for _, num := range nums {
+		newNums = append(newNums, &num)
+	}
+	return newNums
 }
